0fd1d1faa8f7a6f4b6229735261e149c148153d3
nawal2002ak-la <nawal2002.ak@gmail.com>
2026-02-10 15:42:46 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,30 +1,26 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './component/layout';
 import Landingpage from './component/Landingpage';
+
 function App() {
 
-const router=createBrowserRouter([
-{
-path:"/", 
-element:<Layout/>,
-children:[
-{index: true ,element:<Landingpage/>},
-
-],
-
-}
-
-]);
-
-
-
-
-  return (
-    <>
-    <RouterProvider router={router}/>
-    </>
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          { index: true, element: <Landingpage /> },
+        ],
+      },
+    ],
+    {
+      basename: "/profile", 
+    }
   );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
